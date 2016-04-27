c7da0c2f192981067f2380149beed606e1b4dad7
liyahui <liyahui.cn@gmail.com>
2016-04-27 23:55:02 +0800

    添加注释

--- set/set.js
+++ set/set.js
@@ -1,6 +1,10 @@
+/**
+ * 集合是由一组无序且唯一的项组成
+ */
 function Set() {
 	var items = {}
 
+	// 向集合添加一个新的值
 	this.add = function(value) {
 		if (!this.has(value)) {
 			items[value] = value
@@ -9,6 +13,7 @@ function Set() {
 		return false
 	}
 
+	// 从集合移除一个值
 	this.remove = function(value) {
 		if (this.has(value)) {
 			delete items[value]
@@ -17,18 +22,22 @@ function Set() {
 		return false
 	}
 
+	// 如果值在集合中返回true，否则返回false
 	this.has = function(value) {
 		return items.hasOwnProperty(value)
 	}
 
+	// 移除集合中所有值
 	this.clear = function() {
 		items = {}
 	}
 
+	// 返回集合中元素的数量
 	this.size = function() {
 		return this.values().length
 	}
 
+	// 返回一个包涵集合中所有值的数组 
 	this.values = function() {
 		return Object.keys(items)
 	}
@@ -39,7 +48,7 @@ var set = new Set()
 console.log(set.add(1)) // true
 console.log(set.has(1)) // true 
 console.log(set.values()) // [1]
-console.log(set.size())	// 1
+console.log(set.size()) // 1
 
 console.log(set.add(2)) // true
 console.log(set.has(2)) // true
