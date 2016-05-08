fe301440fedcd90b82e1aef954f60be756f5d324
liyahui <liyahui.cn@gmail.com>
2016-05-08 22:51:03 +0800

    格式化

--- 队列/format.js
+++ 队列/format.js
@@ -1,65 +1,73 @@
 /**
- * 队列是一种遵循先进先出原则的一组有序集合
- * 队列在末尾增加新元素，并从顶部移除元素
- * 最新添加的元素必须排在队列的末尾
+ * 优先队列的添加和移除是基于优先级的
  */
-function Queue() {
-	var items = [];
+function PriorityQueue() {
+	var items = []
 
-	// 向队列尾部添加元素
-	this.enqueue = function(element) {
-		items.push(element);
+	// 按照优先级添加元素
+	this.enqueue = function(element, priority) {
+		var queueElement = {
+			element: element,
+			priority: priority
+		}
+
+		if (this.isEmpty()) {
+			items.push(queueElement)
+		} else {
+			var added = false
+			for (var i = 0; i < items.length; i++) {
+				if (queueElement.priority < items[i].priority) {
+					items.splice(i, 0, queueElement)
+					added = true
+					break
+				}
+			}
+
+			if (!added) {
+				items.push(queueElement)
+			}
+		}
 	}
 
 	// 移除队列第一个元素，并返回被移除的元素
 	this.dequeue = function() {
-		return items.shift();
+		return items.shift()
 	}
 
 	// 返回队列的第一个元素，队列不做任何变动
 	this.front = function() {
-		return items[0];
+		return items[0]
 	}
 
 	// 如果队列不包含任何元素返回true，否则返回false
 	this.isEmpty = function() {
-		return items.length === 0;
+		return items.length === 0
 	}
 
 	// 移除队列里所有元素
 	this.clear = function() {
-		items = [];
+		items = []
 	}
 
 	// 返回队列中包含的元素个数
 	this.size = function() {
-		return items.length;
+		return items.length
 	}
 
 	// 辅助方法，返回队列内元素
 	this.print = function() {
-		return items.toString();
+		return items
 	}
 }
 
-var queue = new Queue();
+var priorityQueue = new PriorityQueue()
 
-console.log(queue.isEmpty()); // true
+priorityQueue.enqueue('a', 3)
+priorityQueue.enqueue('b', 4)
+priorityQueue.enqueue('c', 1)
+priorityQueue.enqueue('d', 2)
+priorityQueue.enqueue('e', 0)
+priorityQueue.enqueue('f', 8)
+priorityQueue.enqueue('g', 3)
 
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.isEmpty()); // false
-
-console.log(queue.print()); // 1, 2, 3
-
-queue.dequeue();
-
-console.log(queue.print()); // 2, 3
-
-console.log(queue.front()); // 2
-
-queue.clear();
-
-console.log(queue.print()); //
+console.log(priorityQueue.print())
